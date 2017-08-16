801e0db3cb77b38f22441ea0a0cf06614fe72efd
Nour-Eddine ECH-CHEBABY <ech.chebaby@outlook.com>
2017-08-16 13:12:19 +0100

    Validation errors and old data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,20 +36,30 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="/threads">All Threads</a></li>
                         <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    Channels <span class="caret"></span>
-                                </a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                Threads <span class="caret"></span>
+                            </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    @foreach(\App\Channel::all() as $channel)
-                                        <li>
-                                            <a href="/threads/{{$channel->slug}}"> {{ $channel->name }} </a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            </li>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="/threads">All</a></li>
+                                <li class="divider"></li>
+                                <li><a href="/threads/create">Create Thread</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                Channels <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                @foreach(\App\Channel::all() as $channel)
+                                    <li>
+                                        <a href="/threads/{{$channel->slug}}"> {{ $channel->name }} </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -11,18 +11,38 @@
 					</div>
 
 					<div class="panel-body">
+
+					@if(count($errors))
+						<div class="alert alert-danger">
+							@foreach($errors->all() as $error)
+								<li>{{ $error }}</li>
+							@endforeach
+						</div>
+					@endif
 						
-						<form action="{{ route('threads.store')}}" method="POST" role="form">
+						<form action="/threads" method="POST" role="form">
 							{{ csrf_field() }}
+							
+							<div class="form-group">
+								<label for="channel_id">Channel:</label>
+								<select name="channel_id" id="channel_id" class="form-control" required>
+									<option value="">Choose channel...</option>
+									@foreach(\App\Channel::all() as $channel)
+										<option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+											{{ $channel->name }}
+										</option>
+									@endforeach
+								</select>
+							</div>
 						
 							<div class="form-group">
 								<label for="title">Title:</label>
-								<input type="text" class="form-control" id="title" name="title" placeholder="Title">
+								<input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{ old('title') }}" required>
 							</div>
 
 							<div class="form-group">
 								<label for="body">Body:</label>
-								<textarea class="form-control" id="body" name="body" rows="5" placeholder="Start typing"></textarea>
+								<textarea class="form-control" id="body" name="body" rows="5" placeholder="Start typing" required>{{ old('body') }}</textarea>
 							</div>
 						
 							<button type="submit" class="btn btn-primary">Publish</button>
